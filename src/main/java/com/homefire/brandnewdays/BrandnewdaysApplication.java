63e319602120f2a769034151690675e20109f806
kjj7999 <kjj7999@gmail.com>
2021-05-19 12:44:56 +0900

    initialize spring boot

---
+++ src/main/java/com/homefire/brandnewdays/BrandnewdaysApplication.java
@@ -0,0 +1,13 @@
+package com.homefire.brandnewdays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandnewdaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandnewdaysApplication.class, args);
+	}
+
+}
